38e99ae805fd53de34c24fbeacba53021376b66a
Srirangan <srirangan@gmail.com>
2014-03-27 12:39:36 +0100

    - `create_element` - handle namespaces and ids along with class names - `get_bounds` - IE8 friendly get bounds

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,14 +1,31 @@
-/* global window, dom_js, _*/
+/* global window, dom_js, _ */
 (function () {
     "use strict";
 
+    var namespaces = { svg: "http://www.w3.org/2000/svg" };
+
     window.dom_js = {};
 
-    dom_js.create_element = function (tag, attributes, children, events_and_listeners) {
+    dom_js.key_codes = {
+        escape: 27,
+        enter : 13,
+        tab   : 9,
+        left  : 37,
+        up    : 38,
+        right : 39,
+        down  : 40
+    };
+
+    dom_js.create_element = function (name, attributes, children, events_and_listeners) {
         var element;
-        tag = tag.split(".");
-        element = document.createElement(tag[0]);
-        element.className = tag[1];
+        name = parse_name(name);
+        if (name.namespace) {
+            element = document.createElementNS(name.namespace, name.tag);
+            element.namespace = name.namespace;
+        }
+        else element = document.createElement(name.tag);
+        if (name.class_name) element.className = name.class_name;
+        if (name.id) element.id = name.id;
         dom_js.set_attributes(element, attributes);
         dom_js.append_children(element, children);
         dom_js.add_event_listeners(element, events_and_listeners);
@@ -60,15 +77,20 @@
     dom_js.append_children = function (parent, children) { _.each(children, function (child) { dom_js.append_child(parent, child); }); };
 
     dom_js.append_child = function (parent, child) {
-        if (typeof child === "string") child = document.createTextNode(child);
+        if (typeof child === "string") {
+            if (parent.namespace === namespaces.svg) child = document.createTextNode(child, true);
+            else child = document.createTextNode(child);
+        }
         parent.appendChild(child);
     };
 
-    dom_js.remove_element = function (element) { if (element.parentNode) element.parentNode.removeChild(element); };
+    dom_js.remove_element = function (element) { if (element && element.parentNode) element.parentNode.removeChild(element); };
 
     dom_js.remove_elements = function (elements) { _.each(elements, function (element) { dom_js.remove_element(element); }); };
 
-    dom_js.empty_element = function (element) { element.innerHTML = ""; };
+    dom_js.empty_element = function (element) {
+        while (element.firstChild) element.removeChild(element.firstChild);
+    };
 
     dom_js.set_attributes = function (element, attributes) {
         for (var key in attributes) if (attributes.hasOwnProperty(key)) element.setAttribute(key, attributes[key]);
@@ -99,13 +121,59 @@
         return {width: w, height: h};
     };
 
+    dom_js.get_bounds = function (element) {
+        var native_bounds = element.getBoundingClientRect(),
+            bounds = {
+                x     : native_bounds.x,
+                y     : native_bounds.y,
+                width : native_bounds.width,
+                height: native_bounds.height,
+                top   : native_bounds.top,
+                left  : native_bounds.left,
+                bottom: native_bounds.bottom,
+                right : native_bounds.right
+            };
+        if (!bounds.height) bounds.height = bounds.bottom - bounds.top;
+        if (!bounds.width) bounds.width = bounds.right - bounds.left;
+        return bounds;
+    };
+
     dom_js.is_within_bounds = function (child, container) {
-        var child_bounds = child.getBoundingClientRect(),
-            container_bounds = container.getBoundingClientRect(),
+        var child_bounds = dom_js.get_bounds(child),
+            container_bounds = dom_js.get_bounds(container),
             child_center_x = child_bounds.left + (child_bounds.width / 2),
             child_center_y = child_bounds.top + (child_bounds.height / 2);
         return ((child_bounds.left >= container_bounds.left && child_bounds.left <= container_bounds.right) && (child_bounds.top >= container_bounds.top && child_bounds.top <= container_bounds.bottom)) ||
             ((child_center_x >= container_bounds.left && child_center_x <= container_bounds.right) && (child_center_y >= container_bounds.top && child_center_y <= container_bounds.bottom));
     };
+
+    function parse_name(arg) {
+        var split, arg_copy = arg,
+            namespace, tag, class_name, id;
+        if (arg_copy.indexOf(":") > -1) {
+            split = arg_copy.split(":");
+            namespace = split[0];
+            arg_copy = split[1];
+        }
+        if (arg_copy.indexOf("#") > -1) {
+            split = arg_copy.split("#");
+            tag = split[0];
+            arg_copy = split[1];
+        }
+        if (arg_copy.indexOf(".") > -1) {
+            split = arg_copy.split(".");
+            if (tag) id = split[0];
+            else tag = split[0];
+            class_name = split[1];
+        }
+        if (!tag) tag = arg_copy;
+        if (!tag) throw new Error("failed to create element for: " + arg);
+        return {
+            namespace : namespaces[namespace],
+            tag       : tag,
+            class_name: class_name,
+            id        : id
+        };
+    }
 
 })();
